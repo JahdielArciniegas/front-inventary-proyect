1d7b2ad6b7c6811a8a6477c50fcd6f2e4be50ad6
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-04-05 09:18:17 -0400

    Simplificando componente App: Haciendo que todos los estados de la aplicación se encuentren elarchivos hijos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { useState } from "react"
-import loginService from "./service/login"
-import ingredientsService from "./service/ingredients"
-import recipesService from "./service/recipes"
 import { Login } from "./components/Login"
 import Page from "./pages/Page"
 import { BrowserRouter as Router,Link } from "react-router"
-import { useDispatch, useSelector } from "react-redux"
-import { login } from "./reducers/userReducer"
+import { useSelector } from "react-redux"
 import { RootState } from "./store"
-import { setIngredients } from "./reducers/ingredientsReducer"
-import { setRecipes } from "./reducers/recipesReducer"
 
 export interface User {
   username: string
@@ -25,34 +18,7 @@ export interface UserState {
 
 function App() {
   const user = useSelector((state : RootState) => state.user)
-  const dispatch = useDispatch()
-  const [username, setUsername] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
-
-  const handleUsername = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value)
-  }
-  const handlePassword = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
-  const submit = async (e : React.FormEvent) => {
-    e.preventDefault()
-    try{
-      const user = await loginService.login(username, password)
-      const ingredients = await ingredientsService.getUserIngredients(user.id)
-      const recipes = await recipesService.getUserRecipes(user.id)
-      localStorage.setItem('loggedInventaryUser', JSON.stringify(user))
-      recipesService.setTokenRecipes(user.token)
-      ingredientsService.setTokenIngredients(user.token)
-      dispatch(setIngredients(ingredients))
-      dispatch(setRecipes(recipes))
-      dispatch(login(user))
-      setUsername("")
-      setPassword("")
-    } catch (error){
-      console.log(error)
-    }
-  } 
+  
 
   return (
     <>
@@ -63,7 +29,7 @@ function App() {
         <Link to="/user">User</Link>
         <Link to="/ingredients">Ingredients</Link>
       </nav>
-      {!user.user ? <Login submit={submit} username={username} handleUsername={handleUsername} password={password} handlePassword={handlePassword}/> : <Page/> }
+      {!user.user ? <Login/> : <Page/> }
     </Router>
     </>
   )
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,41 @@
-interface LoginProps {
-  submit: (e: React.FormEvent) => void;
-  username: string;
-  handleUsername: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  password: string;
-  handlePassword: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../reducers/userReducer";
+import { setIngredients } from "../reducers/ingredientsReducer";
+import { setRecipes } from "../reducers/recipesReducer";
+import loginService from "../service/login";
+import ingredientsService from "../service/ingredients";
+import recipesService from "../service/recipes";
 
-export const Login = ({submit,username,handleUsername,password,handlePassword}: LoginProps) => {
+export const Login = () => {
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const dispatch = useDispatch()
+
+  const handleUsername = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }
+  const handlePassword = (e : React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+  const submit = async (e : React.FormEvent) => {
+    e.preventDefault()
+    try{
+      const user = await loginService.login(username, password)
+      const ingredients = await ingredientsService.getUserIngredients(user.id)
+      const recipes = await recipesService.getUserRecipes(user.id)
+      localStorage.setItem('loggedInventaryUser', JSON.stringify(user))
+      recipesService.setTokenRecipes(user.token)
+      ingredientsService.setTokenIngredients(user.token)
+      dispatch(setIngredients(ingredients))
+      dispatch(setRecipes(recipes))
+      dispatch(login(user))
+      setUsername("")
+      setPassword("")
+    } catch (error){
+      console.log(error)
+    }
+  } 
   return (
       <div className="form-login" onSubmit={submit}>
         <h2>Inicio de Sesión</h2>
