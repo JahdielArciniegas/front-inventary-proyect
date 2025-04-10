5a5b07dd3df94e1090e8615732d8646286383291
JahdielArciniegas <arciniegasjahdiel@gmail.com>
2025-04-10 19:35:15 -0400

    Diseño de receta perfeccionado y listo

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,12 +1,14 @@
-import { Recipe } from "../reducers/recipesReducer"
+import { Recipe as RecipeType } from "../reducers/recipesReducer"
 import styles from "./Recipe.module.css"
-const Recipe = ({recipe,remove}: {recipe : Recipe, remove : (id:string) => void}) => {
+const Recipe = ({recipe,remove}: {recipe : RecipeType, remove : (id:string) => void}) => {
   return (
     <div className={styles.recipe}>
+      <div className={styles.button}>
+        <button onClick={() => remove(recipe.id)}>x</button>
+      </div>
       <header>{recipe.title}</header>
       <table><thead><tr><th>Nombre</th><th>Cantidad</th></tr></thead><tbody>{recipe.ingredients.map(i => <tr key={i.id}><td>{i.ingredient.name}</td>  <td>{i.amount}</td></tr>)}</tbody></table>
       <footer>Dolar :{recipe.cost} - Pesos :{Number(recipe.cost)*4000}</footer>
-      <button onClick={() => remove(recipe.id)}>x</button>
     </div>
   )
 }
